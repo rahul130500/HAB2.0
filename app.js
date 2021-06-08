48e4c65696f0438ca384ea76f7a44f6d97c4eebd
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-06-08 13:42:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
+
+//home page
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+app.listen(3000, function () {
+  console.log("server running successfully on port 3000");
+});
