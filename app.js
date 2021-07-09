a8575325016402832e43cc4a560e8f081ff7bb3a
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-07-09 16:47:30 +0530

    issue fixed

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const flash = require("connect-flash");
 const hmcRoutes = require("./routes/hmc.routes");
 const announcementsRoutes = require("./routes/announcements.routes");
-const authRoutes= require('./routes/auth-routes');
+const authRoutes = require("./routes/auth-routes");
 const profileRoutes = require("./routes/profile-routes");
 const adminRoutes = require("./routes/admin.routes");
 const app = express();
-const passportSetup = require('./config/passport-setup');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
+const passportSetup = require("./config/passport-setup");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const NoticeRoutes = require("./routes/notice-routes");
 const MessRoutes = require("./routes/mess-routes");
 
 const methodOverride = require("method-override");
-require('dotenv').config();
-const {MONGO_URL} = process.env;
+require("dotenv").config();
+const { MONGO_URL } = process.env;
 
 //const NoticeAdd = require("./routes/noticeadd.routes");
 mongoose
@@ -35,37 +35,33 @@ mongoose
 //harsh's mongodb
 //mongoose.connect("mongodb+srv://Harsh:harsh1234@cluster0.bkld1.mongodb.net/habhmc?retryWrites=true&w=majority", {useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false});
 
-
-app.use(cookieSession({
-    maxAgr:24*60*60*1000,
-    keys:[keys.session.cookieKey]
-}));
+app.use(
+  cookieSession({
+    maxAgr: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey],
+  })
+);
 app.use(flash());
-
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.urlencoded({extended:true}))
-
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use('/auth',authRoutes);
-app.use('/profile',profileRoutes);
-app.use('/admin/notice',NoticeRoutes);
-app.use('/admin/mess',MessRoutes);
-app.use('/admin/hmc',hmcRoutes);
-app.use('/admin/announcements',announcementsRoutes);
-app.use('/admin',adminRoutes);
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+app.use("/admin/notice", NoticeRoutes);
+app.use("/admin/mess", MessRoutes);
+app.use("/admin/hmc", hmcRoutes);
+app.use("/admin/announcements", announcementsRoutes);
+app.use("/admin", adminRoutes);
 
-
-
-
-app.get('/',(req,res)=>{
-   res.render('home',{user:req.user});
+app.get("/", (req, res) => {
+  res.render("home", { user: req.user });
 });
 
-app.listen(5050, ()=>{
-    console.log('app listening 5050');
+app.listen(5050, () => {
+  console.log("app listening 5050");
 });
-
